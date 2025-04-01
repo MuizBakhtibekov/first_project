6d0f580dfda9bab40c488d84ab5245b68a74ec39
Муъиз Бахтибеков <muizb@yandex.ru>
2025-04-02 02:58:39 +0300

    add docstring

--- match_case.py/match_case.py
+++ match_case.py/match_case.py
@@ -1,3 +1,4 @@
+''''Пример работы с match case.'''
 def http_error(status):
     match status:
         case 400:
@@ -10,3 +11,4 @@ def http_error(status):
             return "Something's wrong with the internet"
 if __name__ =='__main__':
     print(http_error(418))
+    pass
